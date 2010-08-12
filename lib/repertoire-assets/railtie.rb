698b522f8f7602e4cf9812c3465548d9d85fad62
Christopher York <yorkc@alexa-pc.home>
2010-08-12 15:27:41 +0100

    Start middleware after Rails3 configuration.
    (Configuration over-rides weren't being loaded from Rails3 environment properly.)

--- lib/repertoire-assets/railtie.rb
+++ lib/repertoire-assets/railtie.rb
@@ -5,7 +5,9 @@ module Repertoire
     class Railtie < Rails::Railtie
       config.repertoire_assets = ActiveSupport::OrderedOptions.new
       
-      config.app_middleware.use Repertoire::Assets::Processor, config.repertoire_assets, Rails.logger
+      initializer "repertoire_assets" do
+        config.app_middleware.use Repertoire::Assets::Processor, config.repertoire_assets, Rails.logger
+      end
       
       rake_tasks do
         dir = Pathname(__FILE__).dirname.expand_path
